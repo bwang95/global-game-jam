ffdd75c785afb9865b0bbe872072e17d1ee8edc5
bwang95 <b.wang@utexas.edu>
2014-01-24 23:54:34 -0600

    Created corner and edge prefabs

---
+++ src/Assets/SCRIPTS/GLOBAL/XMLIO.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+
+public class XMLIO : MonoBehaviour {
+
+    private XmlTextReader xread;
+    private XmlTextWriter xwrite;
+
+    string filename = "";
+    bool reading;
+    string[] write;
+
+	// Use this for initialization
+	void Start () {
+        if (filename.Equals(""))
+        {
+            throw new UnassignedReferenceException("Missing filename!");
+            return;
+        }
+
+        if (reading)
+        {
+            xread = new XmlTextReader(filename);
+            ArrayList list = new ArrayList();
+            
+        }
+        else
+        {
+            xwrite = new XmlTextWriter(filename, System.Text.Encoding.UTF8);
+            for (int k = 0; k < write.Length; k++)
+                xwrite.WriteString(write[k]);
+        }
+        
+	}
+}
