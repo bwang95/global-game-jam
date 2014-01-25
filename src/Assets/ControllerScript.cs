f76a391ac85e165eb278e7f762f6471fcd170045
bwang95 <b.wang@utexas.edu>
2014-01-24 21:07:27 -0600

    Attempting to add controller script

--- src/Assets/ControllerScript.cs
+++ src/Assets/ControllerScript.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class ControllerScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    int speed = 5;
+
 	// Update is called once per frame
 	void Update () {
-	
+        float x = 0, y = 0;
+        if (Input.GetKey(KeyCode.A))
+            x -= 1;
+        else if (Input.GetKey(KeyCode.D))
+            x += 1;
+
+        if (Input.GetKey(KeyCode.W))
+            y += 1;
+        else if (Input.GetKey(KeyCode.S))
+            y -= 1;
+
+        rigidbody.velocity = new Vector3(x * speed, y * speed);
 	}
 }
