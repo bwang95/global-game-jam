7b667877ab0f645c98ea8c62397ac5f2d2fe3d31
bwang95 <b.wang@utexas.edu>
2014-01-25 01:50:53 -0600

    Corrected room spawning code.

--- src/Assets/SCRIPTS/GLOBAL/TestGridSpawn.cs
+++ src/Assets/SCRIPTS/GLOBAL/TestGridSpawn.cs
@@ -3,29 +3,36 @@ using System.Collections;
 
 public class TestGridSpawn : MonoBehaviour {
 
-	public GameObject Token;
+	public GameObject CornerToken;
 	public GameObject WallToken;
+    public GameObject InteriorToken;
+    private Vector3 zVec = new Vector3(0, 0, 1);
 
 public int spacing = 30;
 
 	void Start () {
 		for(int i = -5; i <= 5; i++){
 			for(int n = -5; n <= 5; n++){
-				Vector3 locToSpawn = new Vector3(i*spacing,n*spacing,0);
-				if(i == -5){
-					Instantiate(WallToken, locToSpawn, Quaternion.identity);
-				}
-				if (i == 5){
-					Instantiate(WallToken, locToSpawn, new Quaternion(0,0,180,0));
-				}
-				if (n == -5){
-					Instantiate(WallToken, locToSpawn, new Quaternion(0,0,90,0));
-				}
-				if (n == 5){
-					Instantiate(WallToken, locToSpawn, new Quaternion(0,0,270,0));
-				}
+				Vector3 locToSpawn = new Vector3(i * spacing,n * spacing,0);
+                if (i == -5 && n == -5)
+                    Instantiate(CornerToken, locToSpawn, Quaternion.AngleAxis(90, zVec));
+                else if (i == 5 && n == -5)
+                    Instantiate(CornerToken, locToSpawn, Quaternion.AngleAxis(180, zVec));
+                else if (i == -5 && n == 5)
+                    Instantiate(CornerToken, locToSpawn, Quaternion.identity);
+                else if (i == 5 && n == 5)
+                    Instantiate(CornerToken, locToSpawn, Quaternion.AngleAxis(270, zVec));
 
-				Instantiate(Token, locToSpawn, Quaternion.identity);
+
+				else if (i == -5)
+					Instantiate(WallToken, locToSpawn, Quaternion.identity);
+				else if (i == 5)
+					Instantiate(WallToken, locToSpawn, Quaternion.AngleAxis(180, zVec));
+				else if (n == -5)
+                    Instantiate(WallToken, locToSpawn, Quaternion.AngleAxis(90, zVec));
+				else if (n == 5)
+                    Instantiate(WallToken, locToSpawn, Quaternion.AngleAxis(270, zVec));
+				else Instantiate(InteriorToken, locToSpawn, Quaternion.identity);
 			}
 		}
 	}
