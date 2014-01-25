57fe835ad75e82541e39e1d39e65ed842122991d
bwang95 <b.wang@utexas.edu>
2014-01-25 01:22:16 -0600

    Stashing TestGridSpawn

--- src/Assets/SCRIPTS/GLOBAL/TestGridSpawn.cs
+++ src/Assets/SCRIPTS/GLOBAL/TestGridSpawn.cs
@@ -3,15 +3,45 @@ using System.Collections;
 
 public class TestGridSpawn : MonoBehaviour {
 
-	public GameObject Token;
+	public GameObject centerToken, sideToken, cornerToken;
 	public int spacing = 30;
+    public int gridSize = 5;
 
+    private Vector3 zVec = new Vector3(0, 0, 1);
+    private int loop;
 	void Start () {
-		for(int i = -5; i <= 5; i++){
-			for(int n = -5; n <= 5; n++){
+        loop = gridSize - 1;
+		for(int i = -loop; i <= loop; i++){
+			for(int n = -loop; n <= loop; n++){
 				Vector3 locToSpawn = new Vector3(i*spacing,n*spacing,0);
-				Instantiate(Token, locToSpawn, Quaternion.identity);
+				Instantiate(centerToken, locToSpawn, Quaternion.identity);
 			}
 		}
+
+        for(int col = -loop; col <= loop; col++){
+            Vector3 colLoc = new Vector3(0, col * spacing, 0);
+            Instantiate(sideToken, colLoc, Quaternion.identity);
+            colLoc.x = gridSize * spacing;
+            Instantiate(sideToken, colLoc, Quaternion.AngleAxis(180, zVec));
+
+            colLoc.y = 0;
+            colLoc.x = col * spacing;
+            Instantiate(sideToken, colLoc, Quaternion.AngleAxis(90, zVec));
+            colLoc.x = col * spacing;
+            colLoc.y = gridSize * spacing;
+            Instantiate(sideToken, colLoc, Quaternion.AngleAxis(270, zVec));
+        }
+
+        Vector3 cornerLoc = new Vector3(-gridSize, -gridSize, 0);
+        Instantiate(cornerToken, cornerLoc, Quaternion.identity);
+
+        cornerLoc = new Vector3(-gridSize, gridSize, 0);
+        Instantiate(cornerToken, cornerLoc, Quaternion.AngleAxis(90, zVec));
+
+        cornerLoc = new Vector3(gridSize, -gridSize, 0);
+        Instantiate(cornerToken, cornerLoc, Quaternion.AngleAxis(180, zVec));
+        
+        cornerLoc = new Vector3(gridSize, gridSize, 0);
+        Instantiate(cornerToken, cornerLoc, Quaternion.AngleAxis(270, zVec));
 	}
 }
