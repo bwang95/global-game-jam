2f10d252ad0f4d755f22a1e74890731971a20860
popolo <cvernino@tx.rr.com>
2014-01-25 14:12:14 -0600

    Added Framework Scripts
    -Entity Spawner
    -Enemy Position Limiter

---
+++ src/Assets/SCRIPTS/OBJECT/EnemyLimiter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyLimiter : MonoBehaviour {
+
+	Vector3 parentRoom; //This is the POSITION of the parent room, not the object itself.
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setParentRoom(Vector3 v){
+		if(parentRoom == null){
+			parentRoom = v;
+		}
+	}
+
+	boolean isWithinBounds(){
+		int x = parentRoom.x - transform.position.x;
+		int y = parentRoom.y - transform.position.y;
+		if(Mathf.Abs(x) <= 5 && Mathf.Abs(y) <= 5){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
---
+++ src/Assets/SCRIPTS/OBJECT/RoomSpawnController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomSpawnController : MonoBehaviour {
+
+	GameObject room = transform.parent.gameObject;
+	Vector3 spawnOrigin;
+	public GameObject[] toSpawn;
+
+	void Start () {
+		spawnOrigin = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SpawnObject (GameObject g, Vector3 relativePosition){
+		Vector3 spawnPosition = relativePosition + relativePosition;
+		Instantiate(g, spawnPosition, Quaternion.identity);
+		EnemyLimiter s = g.GetComponent(typeof(EnemyLimiter));
+		s.setParentRoom(spawnOrigin);
+	}
+}
