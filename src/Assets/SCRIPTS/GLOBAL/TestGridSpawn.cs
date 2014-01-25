12ea7ef72497ec5d45b18f1845652f5531395349
C. K. Vernino <cvernino@tx.rr.com>
2014-01-25 01:17:38 -0600

    Outer Boundaries
    Started working on the outer boundaries.
    
    -error : top and bottom walls non functional.

--- src/Assets/SCRIPTS/GLOBAL/TestGridSpawn.cs
+++ src/Assets/SCRIPTS/GLOBAL/TestGridSpawn.cs
@@ -4,14 +4,30 @@ using System.Collections;
 public class TestGridSpawn : MonoBehaviour {
 
 	public GameObject Token;
-	public int spacing = 30;
+	public GameObject WallToken;
+
+public int spacing = 30;
 
 	void Start () {
 		for(int i = -5; i <= 5; i++){
 			for(int n = -5; n <= 5; n++){
 				Vector3 locToSpawn = new Vector3(i*spacing,n*spacing,0);
+				if(i == -5){
+					Instantiate(WallToken, locToSpawn, Quaternion.identity);
+				}
+				if (i == 5){
+					Instantiate(WallToken, locToSpawn, new Quaternion(0,0,180,0));
+				}
+				if (n == -5){
+					Instantiate(WallToken, locToSpawn, new Quaternion(0,0,90,0));
+				}
+				if (n == 5){
+					Instantiate(WallToken, locToSpawn, new Quaternion(0,0,270,0));
+				}
+
 				Instantiate(Token, locToSpawn, Quaternion.identity);
 			}
 		}
 	}
+
 }
