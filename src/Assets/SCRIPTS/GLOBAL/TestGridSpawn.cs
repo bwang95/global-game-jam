aab6021723a8fbd3a9ba1c9126684a7227ac35a5
bwang95 <b.wang@utexas.edu>
2014-01-25 15:06:11 -0600

    Working on TestGridSpawn

--- src/Assets/SCRIPTS/GLOBAL/TestGridSpawn.cs
+++ src/Assets/SCRIPTS/GLOBAL/TestGridSpawn.cs
@@ -3,75 +3,80 @@ using System.Collections;
 
 public class TestGridSpawn : MonoBehaviour {
 
-	public GameObject CornerToken;
-	public GameObject WallToken;
-    public GameObject InteriorToken;
-    public int gridSize = 10;
-    public int exitX = 0, exitY = 0;
-    private Vector3 zVec = new Vector3(0, 0, 1);
+	public GameObject OneDoor, TwoDoors, ThreeDoors, FourDoors;
+    public int exitX = 0, exitY = 0, roughRoomCount = 30, roomSize = 30;
 
-public int spacing = 30;
+    private const Vector3 zVec = new Vector3(0, 0, 1);
+    private GameObject[] rooms;
+    private int[] root = { 0, 0 };
+    
 
 	void Start () {
-        int a = gridSize / 2;
+        rooms = new GameObject[] {OneDoor, TwoDoors, ThreeDoors, FourDoors};
 
-        if(!(exitX == 0 || exitX == gridSize || exitY == 0 || exitY == gridSize)){
-            print("Invalid exit specified. Randomizing...");
-            switch ((int)(Random.value * 4))
-            {
-                case 0: 
-                    exitX = 0;
-                    exitY = (int)(Random.value * gridSize);
-                    break;
-                case 1:
-                    exitX = gridSize;
-                    exitY = (int)(Random.value * gridSize);
-                    break;
-                case 2:
-                    exitX = (int)(Random.value * gridSize);
-                    exitY = 0;
-                    break;
-                case 3:
-                    exitX = (int)(Random.value * gridSize);
-                    exitY = gridSize;
-                    break;
+        int rn = random(0, 4);
+        Instantiate(rooms[1], 
+            new Vector3(root[0], root[1], 0),
+            Quaternion.AngleAxis(rn * 90, zVec));
 
-            }
-        }
+        spawnRooms(
+            rn == 1 ? -1 : (rn == 3 ? 1 : 0), 
+            rn == 0 ? -1 : (rn == 2 ? 1 : 0), 
+            rn + (int)(Random.value * 2));
 
-		for(int i = -a; i <= a; i++){
-			for(int n = -a; n <= a; n++){
-				Vector3 locToSpawn = new Vector3(i * spacing,n * spacing,0);
-
-                if (i + a == exitX && n + a == exitY){
-                    if(Mathf.Abs(exitX) == Mathf.Abs(exitY))
-                        Instantiate(WallToken, locToSpawn, Quaternion.AngleAxis((int)(Random.value * 4) * 90 ,zVec));
-                    else
-                        Instantiate(InteriorToken, locToSpawn, Quaternion.identity);
-
-                    print("Inserting exit");
-                }
-                else if (i == -a && n == -a)
-                    Instantiate(CornerToken, locToSpawn, Quaternion.AngleAxis(90, zVec));
-                else if (i == a && n == -a)
-                    Instantiate(CornerToken, locToSpawn, Quaternion.AngleAxis(180, zVec));
-                else if (i == -a && n == a)
-                    Instantiate(CornerToken, locToSpawn, Quaternion.identity);
-                else if (i == a && n == a)
-                    Instantiate(CornerToken, locToSpawn, Quaternion.AngleAxis(270, zVec));
-
-
-                else if (i == -a)
-                    Instantiate(WallToken, locToSpawn, Quaternion.identity);
-                else if (i == a)
-                    Instantiate(WallToken, locToSpawn, Quaternion.AngleAxis(180, zVec));
-                else if (n == -a)
-                    Instantiate(WallToken, locToSpawn, Quaternion.AngleAxis(90, zVec));
-                else if (n == a)
-                    Instantiate(WallToken, locToSpawn, Quaternion.AngleAxis(270, zVec));
-                else Instantiate(InteriorToken, locToSpawn, Quaternion.identity);
-			}
-		}
+        print("Actual number of rooms: " + roomCounter);
 	}
 
+    private int roomCounter = 0;
+    //doorNum = Door you are coming out of; South, East, North, West
+    void spawnRooms(int x, int y, int doorNum)
+    {
+
+        Vector3 spawn = new Vector3(x * roomSize, y * roomSize, 0);
+        if(roomCounter++ >= roughRoomCount){
+            Instantiate(rooms[0], spawn, Quaternion.AngleAxis((doorNum + 1) % 4 * 90, zVec));
+            return;
+        }
+
+        int numDoors = randomIndex();
+        switch (numDoors)
+        {
+            case 0 :
+                Instantiate(rooms[0], spawn, Quaternion.AngleAxis((doorNum + 1) % 4 * 90, zVec));
+                return;
+            case 1 :
+                int r = (doorNum + (int)(Random.value * 2)) % 4;
+                Instantiate(rooms[1], spawn, Quaternion.AngleAxis(r * 90, zVec));
+                int x = (r == 0 ? 0
+                spawnRooms(
+                    x + (r == 0 ),
+                    , 
+                    r + 1);
+                spawnRooms(
+                    ,
+                    ,
+                    r + 2);
+                return;
+            case 2 :
+
+                
+                return;
+            case 3 :
+                Instantiate(rooms[3], spawn, Quaternion.identity);
+                spawnRooms(x, y - 1, 1);
+                spawnRooms(x + 1, y, 2);
+                spawnRooms(x, y + 1, 3);
+                spawnRooms(x - 1, y, 4);
+                return;
+        }
+    }
+    int randomIndex()
+    {
+        return (int)(Random.value * rooms.Length);
+    }
+    int random(int start, int end)
+    {
+        return ((int)(Random.value * (end - start)) + start);
+    }
+    
 }
