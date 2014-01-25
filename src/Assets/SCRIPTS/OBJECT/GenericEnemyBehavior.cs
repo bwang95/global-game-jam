2365c8f9be06c3fcb6cb054e32150350e113a7a8
Ben Lin <ben.lin9@gmail.com>
2014-01-25 13:40:58 -0600

    Init Enemy Behavior Files Added

---
+++ src/Assets/SCRIPTS/OBJECT/GenericEnemyBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericEnemyBehavior : MonoBehaviour {
+
+	enum Direction {UP, DOWN, LEFT, RIGHT};
+
+	private int speed = 5;
+	Direction dir;
+
+	// Use this for initialization
+	void Start () {
+		print ((int)Direction.UP);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
