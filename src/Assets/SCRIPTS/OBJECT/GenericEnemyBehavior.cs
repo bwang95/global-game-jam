1b13bf080f448dbafe468b4e3fae08828459f2b2
Ben Lin <ben.lin9@gmail.com>
2014-01-26 00:44:59 -0600

    Added enemy AI for all characters

--- src/Assets/SCRIPTS/OBJECT/GenericEnemyBehavior.cs
+++ src/Assets/SCRIPTS/OBJECT/GenericEnemyBehavior.cs
@@ -6,14 +6,17 @@ public class GenericEnemyBehavior : MonoBehaviour {
 	private GameObject player;
 	public float SPEED;
 	private EnemyLimiter s;
+	private Vector2 point;
     public Sprite up, down, left, right;
 	private SpriteRenderer renderer;
+	//private Vector3 home;
 
 	void Start(){
 		player = GameObject.FindGameObjectWithTag ("Player");
 		s = gameObject.GetComponent<EnemyLimiter>();
 		SPEED = 5*Time.deltaTime;
         renderer = gameObject.GetComponent<SpriteRenderer>();
+		//home = gameObject.GetComponent<EnemyLimiter>().parentRoom;
 	}
 	void Update () {
 		if (!s.playerWithinBounds ())
@@ -21,9 +24,6 @@ public class GenericEnemyBehavior : MonoBehaviour {
 
 			float dx = player.transform.position.x - transform.position.x;
 			float dy = player.transform.position.y - transform.position.y;
-
-            
-
 			float diag = Mathf.Sqrt (dx * dx + dy * dy);
 			SPEED = 5 * Time.deltaTime;
 
@@ -43,15 +43,29 @@ public class GenericEnemyBehavior : MonoBehaviour {
 							else
 								renderer.sprite = left;
 						}
-
 						transform.Translate (dx / diag * SPEED, dy / diag * SPEED, 0);
-		
 						break;
 				case Character.WIZARD:
-
+						if(diag < 10){
+							dx *= -1;
+							dy *= -1;
+						}
+						transform.Translate (dx / diag * SPEED, dy / diag * SPEED, 0);
 						break;
 				case Character.SHADOW:
-
+						if (diag < 10){
+							break;
+						}
+						if(point.x == 0 && point.y == 0)
+							point = s.randPoint();
+						else{
+							dx = point.x - transform.position.x;
+							dy = point.y - transform.position.y;
+							diag = Mathf.Sqrt (dx * dx + dy * dy);
+							if(diag < 0.5)
+								point = s.randPoint();
+						}
+						transform.Translate (dx / diag * SPEED, dy / diag * SPEED, 0);
 						break;
 		}
 	}
