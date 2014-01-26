18c2de9f346ab7c35520341bcf8345d7c6ed7cac
C. K. Vernino <cvernino@tx.rr.com>
2014-01-26 05:04:29 -0600

    Guaranteed Card Spawning

--- src/Assets/SCRIPTS/GLOBAL/RoomObjective.cs
+++ src/Assets/SCRIPTS/GLOBAL/RoomObjective.cs
@@ -8,18 +8,24 @@ public class RoomObjective : MonoBehaviour {
 
 	GameObject[] allRooms;
 	void Update () {
+		print (hasRun);
 		if(!hasRun){
 			allRooms = GameObject.FindGameObjectsWithTag("Room");
+			print(allRooms.Length);
 			if(allRooms.Length > 0){
 				int no = allRooms.Length;
 				int a = (int) Mathf.Floor(Random.Range(0, no));
+				print ("a = " + a);
 				int b = a;
 				while (a == b){
 					b = (int) Mathf.Floor(Random.Range(0, no));
 				}
+				print ("b = " + b);
 
 				for (int n = 0; n < allRooms.Length; n++){
-					if(n == a){
+					if(getController(allRooms[n]) == null){
+						//ignore
+					}else if(n == a){
 						getController(allRooms[n]).setSpawnItem(1);
 					}else if(n == b){
 						getController(allRooms[n]).setSpawnItem(2);
@@ -27,6 +33,7 @@ public class RoomObjective : MonoBehaviour {
 						getController(allRooms[n]).setSpawnItem(Mathf.FloorToInt(Random.Range(3,items.GetNames(typeof(items)).Length)));
 					}
 				}
+				hasRun = true;
 			}
 		}
 	}
