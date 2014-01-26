f1b07efabe5564218e2cfbb5a2b2a09f98cc75b4
bwang95 <b.wang@utexas.edu>
2014-01-25 23:21:44 -0600

    Player is now spawned by script

---
+++ src/Assets/SCRIPTS/OBJECT/SpawnPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPlayer : MonoBehaviour {
+
+    public GameObject playerPrefab;
+
+	void Start () {
+        Instantiate(playerPrefab, transform.position, Quaternion.identity);
+	}
+
+}
